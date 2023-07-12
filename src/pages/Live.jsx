459d7d212e279051779fa14ede7e7bf62d4fccae
Rooshaan <mirrooshaanrafi@gmail.com>
2023-07-12 17:57:45 +0500

    Screenshare code updated

--- src/pages/Live.jsx
+++ src/pages/Live.jsx
@@ -1,67 +1,104 @@
 import Peer from 'simple-peer';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Replayer } from 'rrweb';
 import { io } from 'socket.io-client';
 
+// const socket = io('http://localhost:3000');
+const socket = io('https://signalling-server-1a58ba404ba9.herokuapp.com/');
+
+const roomName = 'testing';
+
 const Live = () => {
-  //   useEffect(() => {
-  //     var socket = io('http://localhost:3000');
-  //     var roomName = 'SqFR5uoLEUX8Qzuo66xF686qxf23';
+  const [peer, setPeer] = useState(undefined);
 
-  //     const replayer = new Replayer([], {
-  //       liveMode: true,
-  //     });
-  //     replayer.startLive();
-
-  //     socket.on('connect', () => {
-  //       socket.emit('new-user', roomName);
-
-  //       // received from user side
-  //       socket.on('user-event', (data) => {
-  //         console.log('data', data);
-  //         replayer.addEvent(data);
-  //       });
-  //     });
-  //   }, []);
+  function createPeer(data) {
+    const peer = new Peer({ initiator: false, trickle: false });
+    console.warn('new peer created');
+    setPeer(peer);
+    peer.on('signal', (signal) => {
+      console.warn('sending "answer"', signal);
+      socket.emit('returning signal', {
+        signal,
+        room: roomName,
+      });
+    });
+    peer.signal(JSON.stringify(data));
+  }
 
   useEffect(() => {
-    const socket = io(
-      'https://signalling-server-1a58ba404ba9.herokuapp.com/'
-    );
-    const roomName = 'testing';
-
-    const peer = new Peer({ initiator: false, trickle: false });
     const replayer = new Replayer([], {
       liveMode: true,
     });
     replayer.startLive();
 
     socket.on('connect', () => {
-      // Join socket room
+      console.warn('Sockets Initiating');
       socket.emit('join room', roomName);
     });
 
-    // receiving signal
     socket.on('received signal', (data) => {
-      console.warn('receiving signal');
+      console.warn('*Peer Offer Incoming*');
+      createPeer(data);
+    });
 
-      peer.on('signal', (signal) => {
-        setTimeout(() => {
-          console.warn('returning signal');
-          socket.emit('returning signal', {
-            signal,
-            room: roomName,
-          });
-        }, [200]);
+    if (peer) {
+      peer.on('data', (data) => {
+        replayer.addEvent(JSON.parse(data));
       });
 
-      peer.signal(JSON.stringify(data));
-    });
+      peer.on('error', (error) => {
+        console.warn('ERROR CAME ****************', error);
+        peer.destroy();
+        peer.removeAllListeners('signal');
+        socket.removeListener('received signal');
+        replayer.destroy();
+        setPeer(undefined);
+      });
+    }
+  }, [peer]);
 
-    peer.on('data', (data) => {
-      replayer.addEvent(JSON.parse(data));
-    });
-  }, []);
+  // useEffect(() => {
+  //   const peer = new Peer({ initiator: false, trickle: false });
+  //   console.warn('peer', peer);
+
+  //   const replayer = new Replayer([], {
+  //     liveMode: true,
+  //   });
+  //   replayer.startLive();
+
+  //   console.warn('replayer', replayer);
+
+  //   replayer.startLive();
+
+  //   // Set up a listener for real-time updates
+
+  //   db.collection('signal').onSnapshot((snapshot) => {
+  //     const data = snapshot.docChanges()[snapshot.docChanges().length - 1];
+  //     console.warn('data', data.doc.data());
+  //     peer.on('signal', (signal) => {
+  //       console.warn('returning signal');
+  //       returnSignal(signal);
+  //     });
+  //     peer.signal(JSON.stringify(data.doc.data().signal));
+  //   });
+
+  //   peer.on('data', (data) => {
+  //     replayer.addEvent(JSON.parse(data));
+  //   });
+  // }, []);
+
+  // // Return signal to the Initializing peer
+  // function returnSignal(signal) {
+  //   console.warn('signal', signal);
+  //   db.collection('returningSignal')
+  //     .add({ signal })
+  //     .then((docRef) => {
+  //       console.log('Message added with ID:', docRef.id);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error adding message:', error);
+  //     });
+  // }
 
   return <h1>Live Sharing</h1>;
 };
