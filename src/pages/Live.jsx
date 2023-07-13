7fa040424f76b77e88ebade7ecbb69721a09761a
Rooshaan <mirrooshaanrafi@gmail.com>
2023-07-13 13:43:40 +0500

    updated code

--- src/pages/Live.jsx
+++ src/pages/Live.jsx
@@ -1,6 +1,6 @@
 import Peer from 'simple-peer';
 import { useEffect, useState } from 'react';
-import { Replayer } from 'rrweb';
+import { Replayer, unpack } from 'rrweb';
 import { io } from 'socket.io-client';
 
 // const socket = io('http://localhost:3000');
@@ -13,24 +13,21 @@ const Live = () => {
 
   function createPeer(data) {
     const peer = new Peer({ initiator: false, trickle: false });
-    console.warn('new peer created');
     setPeer(peer);
     peer.on('signal', (signal) => {
-      console.warn('sending "answer"', signal);
+      console.warn('sending "answer"', peer._id);
       socket.emit('returning signal', {
         signal,
         room: roomName,
       });
     });
-    peer.signal(JSON.stringify(data));
+    console.warn('peer', peer._id);
+    setTimeout(() => {
+      peer.signal(JSON.stringify(data));
+    }, 1000);
   }
 
   useEffect(() => {
-    const replayer = new Replayer([], {
-      liveMode: true,
-    });
-    replayer.startLive();
-
     socket.on('connect', () => {
       console.warn('Sockets Initiating');
       socket.emit('join room', roomName);
@@ -40,8 +37,17 @@ const Live = () => {
       console.warn('*Peer Offer Incoming*');
       createPeer(data);
     });
+  }, []);
+
+  useEffect(() => {
+    const replayer = new Replayer([], {
+      liveMode: true,
+      unpackFn: unpack,
+    });
+    replayer.startLive();
 
     if (peer) {
+      console.warn('peer', peer._id);
       peer.on('data', (data) => {
         replayer.addEvent(JSON.parse(data));
       });
@@ -50,55 +56,12 @@ const Live = () => {
         console.warn('ERROR CAME ****************', error);
         peer.destroy();
         peer.removeAllListeners('signal');
-        socket.removeListener('received signal');
+        // socket.removeListener('received signal');
         replayer.destroy();
         setPeer(undefined);
       });
     }
   }, [peer]);
-
-  // useEffect(() => {
-  //   const peer = new Peer({ initiator: false, trickle: false });
-  //   console.warn('peer', peer);
-
-  //   const replayer = new Replayer([], {
-  //     liveMode: true,
-  //   });
-  //   replayer.startLive();
-
-  //   console.warn('replayer', replayer);
-
-  //   replayer.startLive();
-
-  //   // Set up a listener for real-time updates
-
-  //   db.collection('signal').onSnapshot((snapshot) => {
-  //     const data = snapshot.docChanges()[snapshot.docChanges().length - 1];
-  //     console.warn('data', data.doc.data());
-  //     peer.on('signal', (signal) => {
-  //       console.warn('returning signal');
-  //       returnSignal(signal);
-  //     });
-  //     peer.signal(JSON.stringify(data.doc.data().signal));
-  //   });
-
-  //   peer.on('data', (data) => {
-  //     replayer.addEvent(JSON.parse(data));
-  //   });
-  // }, []);
-
-  // // Return signal to the Initializing peer
-  // function returnSignal(signal) {
-  //   console.warn('signal', signal);
-  //   db.collection('returningSignal')
-  //     .add({ signal })
-  //     .then((docRef) => {
-  //       console.log('Message added with ID:', docRef.id);
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error adding message:', error);
-  //     });
-  // }
 
   return <h1>Live Sharing</h1>;
 };
